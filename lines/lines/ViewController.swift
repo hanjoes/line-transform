a8654e79969666398eb4404fd3f59d78491e4b53
Hanzhou Shi <hanzhou87@gmail.com>
2016-03-11 10:36:31 -0800

    Created project.

---
+++ lines/lines/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  lines
+//
+//  Created by Hanzhou Shi on 3/11/16.
+//  Copyright © 2016 Hanzhou Shi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
